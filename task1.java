1e8a11f4a2cf671de23d4c223187c478879e0dcd
Nurdaulet <154047316+nurdaau@users.noreply.github.com>
2025-11-19 09:59:49 +0500

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,18 @@
+package labWork;
+
+public class task1 {
+    public static void main(String[] args) {
+        try {
+            int result = divideNumbers(5, 2);
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    public static int divideNumbers(int dividend, int divisor) {
+        if (divisor == 0) {
+            throw new ArithmeticException("Cannot divide the given number by zero!");
+        }
+        return dividend / divisor;
+    }
+}
